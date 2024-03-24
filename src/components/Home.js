8ddc15fe05f9917ee704b8dbc31e8d9e6debbc5a
FANT0M100 <giorgie103188@gmail.com>
2024-03-24 18:05:30 +0400

    about and work page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
 import React from "react";
 import NavBar from "./NavBar";
 
+import bannerBackground from "../assets/home-banner-background.png";
+import BannerImage from "../assets/home-banner-image.png";
+
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <NavBar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={bannerBackground} alt="back image" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Enjoy Tasty Meals, Straight to Your Plate, Always Fresh and Hot!
+          </h1>
+          <p className="primary-text">
+            Dive into our yummy meals, made just for you and brought to your
+            door, so you can enjoy every bite without waiting!
+          </p>
+          <button className="secondary-button">Order Now</button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="banner image" />
+        </div>
+      </div>
     </div>
   );
 };
